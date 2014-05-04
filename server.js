13344cee024bfbbe9dfac6f32b40f911d07fc170
Mark Coleman <markcoleman@gmail.com>
2014-05-04 19:52:58 -0400

    final polish
    fixed the thank you page, cleaned up the server.js, created a base html
    page without angular, and cleaned up some html in the index page.

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-/**
- * Created by colemanm on 3/27/14.
- */
 var express = require('express')
     , app = express()
     , directory = __dirname;
@@ -8,14 +5,11 @@ var express = require('express')
 app.use(express.static(directory))
     .use(express.bodyParser());
 
-app.get('/hello.txt', function(req, res){
-    res.send('Hello World');
-});
 app.post("/save", function(req, res){
     console.log(req.body);
     res.redirect(301, 'thankyou.html');
 });
 
-var server = app.listen(8080,function(){
+app.listen(8080,function(){
     console.log("Listening on 8080 and serving content from " + directory);
 });
